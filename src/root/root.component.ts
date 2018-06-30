7fa5a09c1fb0852dc83d57af39fd59ba6b7da13f
Ian Agpawa <agpawaji@gmail.com>
2018-06-30 15:25:09 -0700

    refactor: top level app, with root

---
+++ src/root/root.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+require('./root.component.scss');
+
+@Component({
+    selector: 'root-app',
+    templateUrl: './root.component.html',
+})
+export class RootComponent { }
