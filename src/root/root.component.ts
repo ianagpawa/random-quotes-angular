ac41a9d85e210113072a8f0a79cbf716b4a3bec1
Ian Agpawa <agpawaji@gmail.com>
2018-07-01 03:41:32 -0400

    feat: Add base for primeng and ag-grid

--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -6,4 +6,19 @@ require('./root.component.scss');
     selector: 'root-app',
     templateUrl: './root.component.html',
 })
-export class RootComponent { }
+export class RootComponent { 
+    title = 'app';
+
+    columnDefs = [
+        {headerName: 'Make', field: 'make' },
+        {headerName: 'Model', field: 'model' },
+        {headerName: 'Price', field: 'price'}
+    ];
+
+    rowData = [
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 }
+    ];
+
+}
--- src/root/root.module.ts
+++ src/root/root.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { RootComponent } from './root.component';
+import { AgGridModule } from 'ag-grid-angular';
+
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        BrowserAnimationsModule,
+        AgGridModule.withComponents([
+
+        ]),
+        CardModule,
+        ButtonModule
     ],
     declarations: [
         RootComponent
