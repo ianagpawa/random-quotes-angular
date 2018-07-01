6735622ab4a1d0f6c10a8edcfc02798c51da74fb
Ian Agpawa <agpawaji@gmail.com>
2018-07-01 09:07:29 -0700

    refactor: Move to templates to compenents

--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -7,18 +7,6 @@ require('./root.component.scss');
     templateUrl: './root.component.html',
 })
 export class RootComponent { 
-    title = 'app';
-
-    columnDefs = [
-        {headerName: 'Make', field: 'make' },
-        {headerName: 'Model', field: 'model' },
-        {headerName: 'Price', field: 'price'}
-    ];
-
-    rowData = [
-        { make: 'Toyota', model: 'Celica', price: 35000 },
-        { make: 'Ford', model: 'Mondeo', price: 32000 },
-        { make: 'Porsche', model: 'Boxter', price: 72000 }
-    ];
+    
 
 }
